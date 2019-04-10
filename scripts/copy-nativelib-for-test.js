2e398cdd5306665c89a1b75558123edfd53fd643
jeremy.smartt@teradata.com <jeremy.smartt@teradata.com>
2019-04-09 23:32:49 -0700

    update tests to use scope

--- scripts/copy-nativelib-for-test.js
+++ scripts/copy-nativelib-for-test.js
@@ -4,7 +4,7 @@ var gulp = require('gulp-help')(require('gulp'));
 var config = {
   paths: {
     requiredfiles: [
-      'node_modules/teradata-nativelib-darwin/**/*'
+      'node_modules/@teradataprebuilt/nativelib-darwin/**/*'
     ]
   }
 };
@@ -12,5 +12,5 @@ var config = {
 gulp.task('copy-native-lib-for-test', 'Copy Native Lib For test', function () {
   return gulp
     .src(config.paths.requiredfiles)
-    .pipe(gulp.dest('src/node_modules/teradata-nativelib-darwin'));
+    .pipe(gulp.dest('src/node_modules/@teradataprebuilt/nativelib-darwin'));
 });
