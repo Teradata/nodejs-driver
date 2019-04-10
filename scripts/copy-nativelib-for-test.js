34c40c3ad276e2896c8da119c3aa49932ef18668
js186150 <jeremy.smartt@teradata.com>
2019-04-10 12:30:35 -0700

    Updates for tests to work on Windows

--- scripts/copy-nativelib-for-test.js
+++ scripts/copy-nativelib-for-test.js
@@ -1,10 +1,13 @@
 'use strict';
 
 var gulp = require('gulp-help')(require('gulp'));
+
+var os = process.platform;
+
 var config = {
   paths: {
     requiredfiles: [
-      'node_modules/@teradataprebuilt/nativelib-darwin/**/*'
+      'node_modules/@teradataprebuilt/nativelib-' + os + '/**/*'
     ]
   }
 };
@@ -12,5 +15,5 @@ var config = {
 gulp.task('copy-native-lib-for-test', 'Copy Native Lib For test', function () {
   return gulp
     .src(config.paths.requiredfiles)
-    .pipe(gulp.dest('src/node_modules/@teradataprebuilt/nativelib-darwin'));
+    .pipe(gulp.dest('src/node_modules/@teradataprebuilt/nativelib-' + os));
 });
