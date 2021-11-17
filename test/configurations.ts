21ccd37d240f3f65d876d70d63efa450965de75c
Adam Tworkiewicz <adam.tworkiewicz@teradata.com>
2021-11-17 17:05:18 -0600

    ci integration tests

--- test/configurations.ts
+++ test/configurations.ts
@@ -1,10 +1,10 @@
 import { ITDConnParams } from '../src/teradata-connection';
 
 export const connParams: ITDConnParams = {
-  host: '<host>',
-  log: '1',
-  password: '<password>',
-  user: '<username>',
+  host: '127.0.0.1',
+  log: '0',
+  password: 'TD01',
+  user: 'TD01',
 };
 
 export const badConnParams: ITDConnParams = {
